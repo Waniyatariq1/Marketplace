1c6f15ac92ee14f8ac52cee7cbb3615bcdd02888
waniyatariq1 <waniyatariq9@gmail.com>
2025-02-06 06:09:49 -0800

    first commit

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,4 +1,5 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import product from './product';
+import { type SchemaTypeDefinition } from 'sanity';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [product],
